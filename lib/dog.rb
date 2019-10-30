6a70178a05414152039eb79a5ada419797c8df9e
imade osifo <imadejr@yahoo.com>
2019-10-30 21:14:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+require "pry"
+
+class Dog 
+  
+  @@all = []
+  @@name = []
+  @@output = ""
+  
+  def initialize(name)
+    @@name << name 
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.clear_all
+    @@all = []    
+  end 
+  
+   def self.print_all 
+     
+     @@name.each do |names| 
+       @@output += names
+     end 
+    # @@output += "#{@name}\n"
+     puts @@output
+   end 
+end 
